e064d71488043408d63d9920c665a959c68e61fe
slbaker <bakerandco@gmail.com>
2013-08-03 15:47:20 -0700

    starting over

--- Shannon.Baker/ReviewApp/ReviewApp/Program.cs
+++ Shannon.Baker/ReviewApp/ReviewApp/Program.cs
@@ -10,6 +10,7 @@ namespace ReviewApp
     {
         private static readonly Decimal NO_PRICE_INFORMATION = -1;
 
+
         static void Main(string[] args)
         {
             String stockToTrack;
